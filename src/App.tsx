8e2ff0208c55c53d97d866783eb9f89c0486242a
duong.nh <d.xuan@humain.co.jp>
2024-11-22 10:15:47 +0700

    FIRST COMMIT

---
+++ src/App.tsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from "react";
+import liff from "@line/liff";
+import "./App.css";
+
+function App() {
+  const [message, setMessage] = useState("");
+  const [error, setError] = useState("");
+  const [token, setToken] = useState("");
+
+  useEffect(() => {
+    liff
+      .init({
+        liffId: import.meta.env.VITE_LIFF_ID,
+      })
+      .then(() => {
+        setMessage("LIFF init succeeded.");
+        const idToken = liff.getAccessToken();
+        // nếu tồn tại thì call api login = token
+        console.log(idToken);
+        setToken(idToken ?? "");
+      })
+      .catch((e: Error) => {
+        setMessage("LIFF init failed.");
+        setError(`${e}`);
+      });
+  });
+
+  return (
+    <div className="App">
+      <h1>create-liff-app</h1>
+      <h1>token: {token}</h1>
+      {message && <p>{message}</p>}
+      {error && (
+        <p>
+          <code>{error}</code>
+        </p>
+      )}
+      <a
+        href="https://developers.line.biz/ja/docs/liff/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        LIFF Documentation
+      </a>
+    </div>
+  );
+}
+
+export default App;
